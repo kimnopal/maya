093a88a96d36b6625f87ea4fd185df29ad2b251e
kimnopal <naufalhakim366@gmail.com>
2024-01-10 12:50:13 +0700

    feat: add relationship between faculty and major model

--- entity/faculty_entity.go
+++ entity/faculty_entity.go
@@ -1,10 +1,11 @@
 package entity
 
 type Faculty struct {
-	ID        uint64 `gorm:"column:id;primaryKey"`
-	Name      string `gorm:"column:username;unique"`
-	CreatedAt uint64 `gorm:"column:created_at;autoCreateTime:milli"`
-	UpdatedAt uint64 `gorm:"column:updated_at;autoCreateTime:milli;autoUpdateTime:milli"`
+	ID        uint64  `gorm:"column:id;primaryKey"`
+	Name      string  `gorm:"column:username;unique"`
+	CreatedAt uint64  `gorm:"column:created_at;autoCreateTime:milli"`
+	UpdatedAt uint64  `gorm:"column:updated_at;autoCreateTime:milli;autoUpdateTime:milli"`
+	Majors    []Major `gorm:"foreginKey:faculty_id;references:id"`
 }
 
 func (f *Faculty) TableName() string {
--- entity/major_entity.go
+++ entity/major_entity.go
@@ -1,10 +1,12 @@
 package entity
 
 type Major struct {
-	ID        uint64 `gorm:"column:id;primaryKey"`
-	Name      string `gorm:"column:name;unique"`
-	CreatedAt uint64 `gorm:"column:created_at;autoCreateTime:milli"`
-	UpdatedAt uint64 `gorm:"column:updated_at;autoCreateTime:milli;autoUpdateTime:milli"`
+	ID        uint64  `gorm:"column:id;primaryKey"`
+	Name      string  `gorm:"column:name;unique"`
+	CreatedAt uint64  `gorm:"column:created_at;autoCreateTime:milli"`
+	UpdatedAt uint64  `gorm:"column:updated_at;autoCreateTime:milli;autoUpdateTime:milli"`
+	FacultyID uint64  `gorm:"column:faculty_id"`
+	Faculty   Faculty `gorm:"foreignKey:faculty_id;references:id"`
 }
 
 func (m *Major) TableName() string {
