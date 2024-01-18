36c6857981486c2a656ea78006ef4131111cc148
kimnopal <naufalhakim366@gmail.com>
2024-01-18 12:02:52 +0700

    feat: add tag model

---
+++ backend/model/tag_model.go
@@ -0,0 +1,23 @@
+package model
+
+type TagResponse struct {
+	ID    uint64 `json:"id"`
+	Value string `json:"value"`
+}
+
+type TagCreateRequest struct {
+	Value string `json:"value" validate:"required,max=100"`
+}
+
+type TagUpdateRequest struct {
+	ID    uint64 `json:"id" validate:"required"`
+	Value string `json:"value" validate:"required,max=100"`
+}
+
+type TagDeleteRequest struct {
+	ID uint64 `json:"id" validate:"required"`
+}
+
+type TagGetRequest struct {
+	Value uint64 `json:"id" validate:"required"`
+}
