03a43d6a3041ac25523c0c572ca6f4cb2fe0b3f9
kimnopal <naufalhakim366@gmail.com>
2024-02-01 23:54:27 +0700

    feat(login): add login middleware for checking token cookie

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,22 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
+import * as jose from "jose";
 
-
-const jwt = require("jsonwebtoken")
-
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
     
     const token = request.cookies.get('token')?.value;
     if (!token) {
         return NextResponse.redirect(new URL("/signin", request.url))
     }
-
-
-    // return NextResponse.redirect(new URL("/signin", request.url))
     
-        // NextResponse.redirect(new URL("/", request.url))
-    // jwt.verify(auth)
-
-    return NextResponse.next()
+    try {
+        await jose.jwtVerify(token, Buffer.from("rahasia", 'utf8'))
+        return NextResponse.next()
+    } catch (error) {
+        return NextResponse.redirect(new URL("/signin", request.url))
+    }
 }
 export const config = {
     matcher: ['/explore'],
